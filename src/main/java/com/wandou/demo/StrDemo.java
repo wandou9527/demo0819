fadb4f8668b243db3be8848717718d64ee173346
liming <limingcm@163.com>
2018-08-21 20:04:27 +0800

    字符串验证

---
+++ src/main/java/com/wandou/demo/StrDemo.java
@@ -0,0 +1,11 @@
+package com.wandou.demo;
+
+public class StrDemo {
+    @Test
+    public void m1() {
+        String s1 = "haha";
+        String s2 = "haha";
+
+        System.out.println(s1 == s2);
+    }
+}
