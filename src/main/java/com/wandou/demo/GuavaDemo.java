37abf5a243e8626163964a726f28f2decfc457cc
liming <liming1@huimin100.cn>
2020-07-31 20:42:36 +0800

    线程池；位运算

--- src/main/java/com/wandou/demo/GuavaDemo.java
+++ src/main/java/com/wandou/demo/GuavaDemo.java
@@ -1,5 +1,6 @@
 package com.wandou.demo;
 
+import com.google.common.base.Joiner;
 import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnels;
 import org.junit.Test;
@@ -13,12 +14,17 @@ import java.nio.charset.StandardCharsets;
  */
 public class GuavaDemo {
 
+    /**
+     * 布隆过滤器
+     */
     @Test
     public void t1() {
         BloomFilter<String> bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charset.forName("utf-8")), 10000, 0.001);
         bloomFilter.put("abc");
         bloomFilter.put("java");
         bloomFilter.put("mysql");
+        bloomFilter.put("redis");
+        bloomFilter.put("spring");
 
         /**
          * 存在为可能存在，不存在一定不存在。因为存在可能是因为哈希冲突
@@ -27,6 +33,16 @@ public class GuavaDemo {
         System.out.println("mightContain = " + mightContain);
         boolean mightContain1 = bloomFilter.mightContain("ab");
         System.out.println("mightContain1 = " + mightContain1);
+        boolean mightContain2 = bloomFilter.mightContain("spring");
+        System.out.println("mightContain2 = " + mightContain2);
+
+    }
+
+    @Test
+    public void tCommon() {
+        Joiner joiner = Joiner.on("-").skipNulls();
+        String join = joiner.join("java", "php", "python", "c");
+        System.out.println("join = " + join);
 
     }
 
